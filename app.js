bf0c54f30a028217d292ac30059329116cd53909
Jack Bogart <bogart.jack98@gmail.com>
2018-09-13 09:21:20 -0600

    Commit 3 - Excited Kittens answered

--- app.js
+++ app.js
@@ -12,4 +12,21 @@
 		}
 	}
 
+//Q3. Love me * 20
 
+	for (let i = 0; i <20;i++){
+		console.log("Love me, pet me! HSSSSSSS!");
+		if(i%2===0){
+			if((3*Math.random()<=1)){
+				console.log("...why does the red dot get away...");
+			}
+		
+			if((3*Math.random()<=2)&&((3*Math.random())>1){
+				console.log("....The catnip made me do it...");
+			}
+
+			if((3*Math.random()<=3)&&((3*Math.random())>2){
+				console.log("...human...why you taking pictures of me");
+			}
+		}
+	}
