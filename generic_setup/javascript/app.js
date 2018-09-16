db95e674450aae38558790b620de72f91ce721e3
MarchWaden <max.linz@live.com>
2018-09-16 13:25:33 -0600

    Did question 1 and 2

---
+++ generic_setup/javascript/app.js
@@ -0,0 +1,13 @@
+console.log("Javascript is working!");
+
+//1.
+for (let i = 1; i <21; i++){
+  console.log(i);
+}
+//2.
+for (let i = 2; i < 200; i++){
+  if(i % 2 = 0){
+    console.log(i);
+  }
+}
+//3.
